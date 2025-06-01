913ce128948d0ed8d7a143e26b90c90e3c3178a0
rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-06-01 18:39:38 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, createContext } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./components/Product";
 import Cart from "./components/Cart";
 import Login from "./components/Login";
-import App1 from "./components/App1";
 import Header from "./components/Header";
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+export const UserContext = createContext();
+
 function App() {
+  const [users, setUsers] = useState([]);
+  const [message, setMessage] = useState("");
+
   return (
-    <div>
+    <UserContext.Provider value={{ users, setUsers, message, setMessage }}>
       <BrowserRouter>
-        
-      
-<Header/>
-      
-          <Routes>
-            <Route index element={<Product />} />
-            <Route path="/" element={<Product/>}></Route>
-            <Route path="/cart" element={<Cart/>}></Route>
-            <Route path="/login" element={<Login/>}></Route>
-            <Route path="/app1" element={<App1/>}></Route>
-          </Routes>
-     
-        <footer>
+        <Header />
+        <Routes>
+          <Route index element={<Product />} />
+          <Route path="/" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <footer style={{ textAlign: "center", marginTop: 40 }}>
           <hr />
           &copy; 2005. All rights Reserved.
         </footer>
       </BrowserRouter>
-    </div>
+    </UserContext.Provider>
   );
 }
+
 export default App;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,28 +1,11 @@
 import React from "react";
 
 export default function Product() {
-  const products = [
-    { id: 1, name: "Product 1", price: 23 },
-    { id: 2, name: "Product 2", price: 30 },
-    { id: 3, name: "Product 3", price: 45 },
-  ];
-  
+  const username = localStorage.getItem("user");
+
   return (
-    <div>
-      <h2>Product List</h2>
-      <div>
-        {products.map((product) => {
-          return (
-            <ol>
-              <li>
-                <div key={product.id}>
-                  {product.name}- $-{product.price}
-                </div>
-              </li>
-            </ol>
-          );
-        })}
-      </div>
+    <div style={{ textAlign: "center", marginTop: 50, fontSize: "20px" }}>
+      Welcome to the Online Store{username ? `, ${username}` : ""}
     </div>
   );
 }
